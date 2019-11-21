a0bfdc1ed2f9b46cc8a1fdeeae2777f85a0f585f
Prerana07 <preranapandit07@gmail.com>
2019-11-21 08:36:59 +0545

    Hashmap applied

--- app/src/main/java/com/example/aad_topic4/MainActivity.java
+++ app/src/main/java/com/example/aad_topic4/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.aad_topic4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    public  static final String countries[] = {
-      "Nepal", "India", "China", "UK", "Canada", "US"
+    public  static String countries[] = {
+      "Nepal","Kathmandu","India","Delhi", "China","Beijing", "UK","London", "Canada","Ottava", "US","Washington, D.C."
     };
+
+    private Map<String, String> dictionary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +28,24 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lsCountries = findViewById(R.id.lvCountries);
 
-       ArrayAdapter adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,countries);
+        dictionary = new HashMap<>();
+        for(int i=0; i<countries.length;i+=2)
+        {
+            dictionary.put(countries[i],countries[i+1]);
+        }
+
+       ArrayAdapter adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,new ArrayList<String>(dictionary.keySet()));
 
         lsCountries.setAdapter(adapter);
+
+//        lsCountries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                String country = parent.getItemAtPosition(position).toString();
+//                String capital = dictionary.get(country);
+//
+//                Toast.makeText(getApplicationContext(),capital.toString(),Toast.LENGTH_LONG).show();
+//            }
+        });
     }
 }
